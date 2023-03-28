94a6b7b36850a13991414005e44d2cdf1b148881
sterlingh78 <sterlingh78@yahoo.com>
2023-03-28 15:57:56 -0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+    const addToDo = () => {
+
+    }
+    const finishEdit = () => {
+        
+    }
+    const markDone = () => {
+        
+    }
+    const editToDo = () => {
+        
+    }
+    const deleteToDo = () => {
+        
+    }
+    const clearDone = () => {
+        
+    }
+</script>
+
+<main>
+    <div class="hero min-h-screen bg-base-300">
+        <div class="hero-content card w-96 bg-base-200 shadow-xl">
+            <div class="flex">
+                <input type="text" placeholder="New todo..." class="input input-bordered input-primary w-full max-w-xs" />
+                <button class="btn btn-primary ml-2">Add</button>
+            </div>
+            <div class="card bg-base-100 p-4 w-full flex-row">
+                <p class="m-auto pr-10">todo name</p>
+                <button class="btn btn-primary">Edit</button>
+                <button class="btn btn-primary ml-2">Delete</button>
+            </div>
+            <button class="btn btn-primary">Clear Done</button>
+        </div>
+    </div>
+</main>
